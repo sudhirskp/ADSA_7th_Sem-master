40bf050aaefcf774ed02ad194829f01490ea6992
sudhirskp <gnitwallah@gmail.com>
2025-07-28 23:15:53 +0530

    Add StringPrint class to read and print string, double, and integer inputs

---
+++ ADSA_7th_Sem-master/src/Hackerrank/StringPrint.java
@@ -0,0 +1,20 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class StringPrint {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // consume newline left-over
+        String s = scan.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scan.close();
+    }
+}
